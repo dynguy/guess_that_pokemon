7d0925556cb6c8060f14d58ca7172a4e802f2e3d
Dylan Nguyen <dynguye@gmail.com>
2021-07-05 23:08:09 -0400

    styling edits

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const apiData = {
+let apiData = {
   url: "https://pokeapi.co/api/v2/",
   type: "pokemon",
-  id: "dragonite",
+  id: "marshtomp",
 };
 
 // destructuring
@@ -9,23 +9,28 @@ const { url, type, id } = apiData;
 
 const apiUrl = `${url}${type}/${id}`;
 
+const searchPokemon = () => {
+  
+}
+
 fetch(apiUrl)
   .then((data) => data.json())
   .then((pokemon) => generateHtml(pokemon));
 
-  const typings = (data) => {
-    if (data.types.length == 1){
-        return data.types[0].type.name
-    } 
-    else{
-        return data.types[0].type.name + " - " + data.types[1].type.name
-    }
+const typings = (data) => {
+  if (data.types.length == 1) {
+    return data.types[0].type.name;
+  } else {
+    return data.types[0].type.name + " - " + data.types[1].type.name;
   }
+};
 
-  const generateHtml = (data) => {
-    console.log(data)
-    const html = `
-        <div class = "name">Pokédex Num ${data.id}. ${data.name}</div> 
+const generateHtml = (data) => {
+  console.log(data);
+  const html = `
+  <div class = "pokedex_container"> 
+  <div class = "name">Pokédex</div>       
+  <div class = "name">Num ${data.id}. ${data.name}</div> 
         <img src=${data.sprites.front_default}>
         <img src=${data.sprites.back_default}>
         <div class = "details_box">
@@ -33,11 +38,11 @@ fetch(apiUrl)
             Type: ${typings(data)}
             </span>    
             <span class = "tag_details">Height: ${data.height / 10} m</span>
-            <span class = "tag_details">Weight: ${data.weight/ 10} kg</span>
+            <span class = "tag_details">Weight: ${data.weight / 10} kg</span>
+            <span class = "tag_details">Generation: ${data.generation}</span>
         </div>
-    `
-    const pokemonDiv = document.querySelector('.pokemon')
-    pokemonDiv.innerHTML = html
-
-
-  }
+</div>
+    `;
+  const pokemonDiv = document.querySelector(".pokemon");
+  pokemonDiv.innerHTML = html;
+};
